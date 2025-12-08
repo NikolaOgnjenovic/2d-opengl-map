aeece8b11357df42f268dfb1494df580f32faef1
Nikola Ognjenovic <nikola.31023@gmail.com>
2025-12-08 22:38:30 +0100

    Added preprocess texture to util.cpp

--- Header/Util.h
+++ Header/Util.h
@@ -2,7 +2,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <string>
+
 int endProgram(std::string message);
+unsigned int compileShader(GLenum type, const char* source);
 unsigned int createShader(const char* vsSource, const char* fsSource);
 unsigned loadImageToTexture(const char* filePath);
 GLFWcursor* loadImageToCursor(const char* filePath);
+void preprocessTexture(unsigned& texture, const char* filepath);
--- src/Util.cpp
+++ src/Util.cpp
@@ -175,3 +175,16 @@ GLFWcursor* loadImageToCursor(const char* filePath) {
         return nullptr;  // Return nullptr on failure
     }
 }
+
+void preprocessTexture(unsigned& texture, const char* filepath) {
+    texture = loadImageToTexture(filepath);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
